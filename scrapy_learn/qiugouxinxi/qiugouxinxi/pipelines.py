dc8f9d83934060367bb85c11a6ef42bc6f8156c8
Yoga-jiang <694234697@qq.com>
2019-10-11 15:27:17 +0800

    add qiugouxinxi spider 2

--- scrapy_learn/qiugouxinxi/qiugouxinxi/pipelines.py
+++ scrapy_learn/qiugouxinxi/qiugouxinxi/pipelines.py
@@ -5,25 +5,41 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymysql
 
 class QiugouxinxiPipeline(object):
-    def open_spider(self, spider):
-        self.file = open('qiugouxinxi.csv', 'w')
+    # def open_spider(self, spider):
+    #     self.file = open('qiugouxinxi.csv', 'w')
+    #
+    # def process_item(self, item, spider):
+    #     if len(item) > 0 :
+    #         # 数据处理的主要方法，在这里面定义对数据的操作
+    #         # 将item强转成字典
+    #         dict_data = dict(item)
+    #         # 将字典转换成json字符串
+    #         # 写入文件
+    #         self.file.write(dict_data['company_name']+"|"+dict_data['hangye']
+    #                         +"|" + dict_data['address']+"|" + dict_data['web']
+    #                         +"|" + dict_data['product']+"|" + dict_data['link']
+    #                         +"|" + dict_data['mobile']+"|" + dict_data['phone']+"\n")
+    #
+    #     return item
+    #
+    # # 在爬虫停止的时候清理一些事情
+    # def close_spider(self, spider):
+    #     self.file.close()
+
+    def __init__(self):
+        self.conn = pymysql.connect('127.0.0.1', 'root', '123456', 'test', charset="utf8", use_unicode=True)
+        self.cursor = self.conn.cursor()
 
     def process_item(self, item, spider):
-        if len(item) > 0 :
-            # 数据处理的主要方法，在这里面定义对数据的操作
-            # 将item强转成字典
-            dict_data = dict(item)
-            # 将字典转换成json字符串
-            # 写入文件
-            self.file.write(dict_data['company_name']+"|"+dict_data['hangye']
-                            +"|" + dict_data['address']+"|" + dict_data['web']
-                            +"|" + dict_data['product']+"|" + dict_data['link']
-                            +"|" + dict_data['mobile']+"|" + dict_data['phone']+"\n")
+        insert_sql = """
+                insert into qiugouxinxi(company_name,hangye,address,web,product,link,mobile,phone)
+                values (%s,%s,%s,%s,%s,%s,%s,%s)
+            """
+        self.cursor.execute(insert_sql, (item["company_name"], item["hangye"], item["address"], item["web"],
+                                         item["product"], item["link"], item["mobile"],
+                                         item["phone"]))
 
-        return item
-
-    # 在爬虫停止的时候清理一些事情
-    def close_spider(self, spider):
-        self.file.close()
+        self.conn.commit()
--- scrapy_learn/qiugouxinxi/qiugouxinxi/spiders/qiugouxinxi_sp.py
+++ scrapy_learn/qiugouxinxi/qiugouxinxi/spiders/qiugouxinxi_sp.py
@@ -23,7 +23,9 @@ class QiugouxinxiSpSpider(scrapy.Spider):
     def start_requests(self):
         base_url = "https://www.qiugouxinxi.net/gs-"
         # 20191009
-        for i in range(3893114, 4185818, 1):
+        # for i in range(3893114, 4185818, 1):
+        # for i in range(3793114, 3893114, 1):
+        for i in range(2693114, 3693114, 1):
             i = str(i+1)
             url = base_url+i+".html"
             print(url)
@@ -32,14 +34,57 @@ class QiugouxinxiSpSpider(scrapy.Spider):
 
     def parse(self, response):
         item = QiugouxinxiItem()
+        company_name = ''
+        hangye = ''
+        address = ''
+        web = ''
+        product = ''
+        link = ''
+        mobile = ''
+        phone = ''
 
         item['company_name'] = response.xpath('/html/body/div[4]/div[2]/div[1]/ul[1]/li[2]/span[2]/text()').extract()[0]
-        item['hangye'] = response.xpath('/html/body/div[4]/div[2]/div[1]/ul[1]/li[1]/a[1]/text()').extract()[0]
-        item['address'] = response.xpath('/html/body/div[4]/div[2]/div[1]/ul[1]/li[3]/span[2]/text()').extract()[0]
-        item['web'] = response.xpath('/html/body/div[4]/div[2]/div[1]/ul[1]/li[4]/span[2]/text()').extract()[0]
-        item['product'] = response.xpath('/html/body/div[4]/div[2]/div[1]/ul[1]/li[5]/span[2]/text()').extract()[0]
-        item['link'] = response.xpath('/html/body/div[4]/div[2]/div[1]/ul[1]/li[6]/span[1]/text()').extract()[0]
-        item['mobile'] = response.xpath('/html/body/div[4]/div[2]/div[1]/ul[1]/li[6]/span[3]/text()').extract()[0].split('/')[0].strip()
-        item['phone'] = response.xpath('/html/body/div[4]/div[2]/div[1]/ul[1]/li[6]/span[3]/text()').extract()[0].split('/')[1].strip()
+        try :
+            hangye = response.xpath('/html/body/div[4]/div[2]/div[1]/ul[1]/li[1]/a[1]/text()').extract()[0]
+        except :
+            hangye = 'error'
+
+        try :
+            address = response.xpath('/html/body/div[4]/div[2]/div[1]/ul[1]/li[3]/span[2]/text()').extract()[0]
+        except :
+            address = 'error'
+
+        try :
+            web = response.xpath('/html/body/div[4]/div[2]/div[1]/ul[1]/li[4]/span[2]/text()').extract()[0]
+        except :
+            web = 'error'
+
+        try :
+            product = response.xpath('/html/body/div[4]/div[2]/div[1]/ul[1]/li[5]/span[2]/text()').extract()[0]
+        except :
+            product = 'error'
+
+        try :
+            link = response.xpath('/html/body/div[4]/div[2]/div[1]/ul[1]/li[6]/span[1]/text()').extract()[0]
+        except :
+            link = 'error'
+
+        try :
+            mobile = response.xpath('/html/body/div[4]/div[2]/div[1]/ul[1]/li[6]/span[3]/text()').extract()[0].split('/')[0].strip()
+        except :
+            mobile = 'error'
+
+        try :
+            phone = response.xpath('/html/body/div[4]/div[2]/div[1]/ul[1]/li[6]/span[3]/text()').extract()[0].split('/')[1].strip()
+        except :
+            phone = 'error'
+
+        item['hangye'] = hangye
+        item['address'] = address
+        item['web'] = web
+        item['product'] = product
+        item['link'] = link
+        item['mobile'] = mobile
+        item['phone'] = phone
 
         yield item
